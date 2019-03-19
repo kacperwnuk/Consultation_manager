66bfb3dff3ccc316097ba6bd50741b47cdd4b26e
Marcin Żyżyński <mzyzynsk@mion.elka.pw.edu.pl>
2019-03-19 13:17:45 +0100

    Init project files

---
+++ Client/Desktop/src/main/java/com/example/tin/Main.java
@@ -0,0 +1,10 @@
+package com.example.tin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
